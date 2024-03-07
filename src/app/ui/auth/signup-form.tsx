5320a1515fdd99b88be08f3cbae4e8c8b38389b9
Stanislav <Stanislavbox24@gmail.com>
2024-03-07 12:47:27 +0200

    add sign up

--- src/app/ui/auth/signup-form.tsx
+++ src/app/ui/auth/signup-form.tsx
@@ -30,6 +30,16 @@ export default function SignUpForm() {
       <div className="w-full border-[2px] rounded-[10px] border-border-secondary dark:border-border-secondary">
         <input
           className="block w-full h-[60px] bg-inherit text-[15px] p-[20px] outline-2 rounded-[10px] placeholder:text-placeholder-color"
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <span className="hidden w-full text-[12px] text-red-600  my-[-15px] px-5">Error</span>
+      </div>
+      <div className="w-full border-[2px] rounded-[10px] border-border-secondary dark:border-border-secondary">
+        <input
+          className="block w-full h-[60px] bg-inherit text-[15px] p-[20px] outline-2 rounded-[10px] placeholder:text-placeholder-color"
           id="password"
           type="password"
           name="password"
@@ -38,12 +48,23 @@ export default function SignUpForm() {
         />
         <span className="hidden w-full text-[12px] text-red-600  my-[-15px] px-5">Error</span>
       </div>
-      <button className=" w-[200px] border-[2px] rounded-[12px] p-3 mt-2 bg-[#00D8D8] text-[#FAFAFA] text-[16px] hover:opacity-40">
+      <div className="w-full border-[2px] rounded-[10px] border-border-secondary dark:border-border-secondary">
+        <input
+          className="block w-full h-[60px] bg-inherit text-[15px] p-[20px] outline-2 rounded-[10px] placeholder:text-placeholder-color"
+          id="confirmPassword"
+          type="password"
+          name="confirmPassword"
+          placeholder="Repeat password"
+          required
+        />
+        <span className="hidden w-full text-[12px] text-red-600  my-[-15px] px-5">Error</span>
+      </div>
+      <button className=" w-[200px] border-[2px] rounded-[12px] p-3 bg-[#00D8D8] text-[#FAFAFA] text-[16px] hover:opacity-40">
         Sign up
       </button>
       <p>Log in with:</p>
       <SocialIcon/>
-      <p className="text-center text-[24px] mt-[30px]">
+      <p className="text-center text-[24px]">
         Have an account?
         <span
           className=" ml-[12px] hover:opacity-40 cursor-pointer text-[#008282]">
