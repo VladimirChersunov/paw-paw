3b1581f1cb2bf2f01ccedccc32a71e5d150e86f4
Stanislav <Stanislavbox24@gmail.com>
2024-03-10 00:55:59 +0200

    add signin

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,11 +1,47 @@
-import AuthLogo from '../ui/auth/logo';
 import UniversalForm from '../ui/auth/universalForm';
+import AuthLogo from '../ui/auth/logo';
+import { InputProps } from '../lib/definitions';
+import SocialIcon from '../ui/auth/social-icon';
+import Link from 'next/link';
 
 export default function SignInPage() {
+  const inputs: InputProps[] = [
+    {
+      type: "text",
+      label: "Username",
+      value: "",
+      name: "username",
+      placeholder: "Username...",
+      error: false,
+      disabled: true,
+      onChange: undefined
+    },
+    {
+      label: "Password",
+      type: "password",
+      placeholder: "Password...",
+      name: "password",
+      value: "",
+      error: false,
+      disabled: true,
+      onChange: undefined
+    },
+  ];
+
   return (
     <main className="flex flex-col bg-fill-background-paw-pals items-center justify-center h-screen">
       <AuthLogo />
-      {/* <UniversalForm /> */}
+      <h1 className="font-semibold text-center text-[40px] leading-[150%] text-ski">Sign in</h1>
+      <UniversalForm inputs={inputs} />
+      <p>Log in with:</p>
+      <SocialIcon />
+      <p className="text-center text-[24px]">
+        Have an account?
+        <span
+          className=" ml-[12px] hover:opacity-40 cursor-pointer text-skin-link">
+          <Link href='/signup'>Sign up!</Link>
+        </span>
+      </p>
     </main>
   );
 }
