697b858dae88429df110108749499b628467d318
Chersunov Vladimir <kb674ua@gmail.com>
2024-03-10 13:54:46 +0200

    get all value and check in console

--- src/app/ui/universalForm.tsx
+++ src/app/ui/universalForm.tsx
@@ -1,44 +1,36 @@
-"use client";
-import Input from "./universalInput";
-import { InputProps } from '../lib/definitions';
-import {FormEvent} from 'react';
-import useInput from "../lib/hooks/inputHook";
+'use client'
+import Input from './universalInput'
+import { InputProps } from '../lib/definitions'
+import { FormEvent } from 'react'
+import useInput from '../lib/hooks/inputHook'
 
 const UniversalForm: React.FC<{ inputs: InputProps[] }> = ({ inputs }) => {
-  
- // Создаем массив экземпляров useInput для каждого input
- const inputFields = inputs.map(input => useInput(input.value || ''));
-
-  
+  // Создаем массив экземпляров useInput для каждого input
+  const inputFields = inputs.map(input => useInput(input.value || ''))
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
 
     // Проходимся по всем полям ввода и валидируем их
-    inputFields.forEach(validateInput);
+    inputFields.forEach(validateInput)
 
     // Создаем новый FormData объект из текущей формы
-  const formData = new FormData(e.currentTarget as HTMLFormElement);
+    const formData = new FormData(e.currentTarget as HTMLFormElement)   
 
+    // Используем formData.get('name') для получения значения каждого поля по имени
+    const username = formData.get('username')
+    const password = formData.get('password')
+    const email = formData.get('email')
+    const rePassword = formData.get('rePassword')
 
-
-  console.log(formData.getAll)
-
-    
-     
-  
-  // Используем formData.get('name') для получения значения каждого поля по имени
-  const username = formData.get('username');
-  const password = formData.get('password');
-
-  
-  
-  // Теперь у вас есть доступ к значениям полей
-  console.log('Username:', username, 'Password:', password);
-    
+    // Теперь у вас есть доступ к значениям полей
+    console.log('Username:', username+'\n', "Email: ", email+'\n', 'Password: ', password+'\n',  "rePassword: ", rePassword+'\n')
   }
 
-  const validateInput = (input: { value: string, setError: (value: boolean) => void }) => {
+  const validateInput = (input: {
+    value: string
+    setError: (value: boolean) => void
+  }) => {
     if (!input.value.trim()) {
       input.setError(true)
     } else {
@@ -47,32 +39,25 @@ const UniversalForm: React.FC<{ inputs: InputProps[] }> = ({ inputs }) => {
   }
 
   return (
-    <form
-    onSubmit={handleSubmit}
-      className="flex flex-col item-center gap-2">
-      <h1 className="text-center text-xl uppercase pb-3">Sign Up</h1>
+    <form onSubmit={handleSubmit} className='flex flex-col item-center gap-2'>
+      <h1 className='text-center text-xl uppercase pb-3'>Sign Up</h1>
       {inputFields.map((input, index) => (
         <Input
           key={index}
           {...inputs[index]}
-           value={input.value}
+          value={input.value}
           error={input.error}
           onChange={input.onChange}
         />
       ))}
       <button
-        type="submit"
-        className="bg-white/20 p-3 mt-2 rounded-md hover:bg-white/40 text-xs">
+        type='submit'
+        className='bg-white/20 p-3 mt-2 rounded-md hover:bg-white/40 text-xs'
+      >
         Submit
       </button>
     </form>
-  );
-};
+  )
+}
 
-export default UniversalForm;
-
-
-
-
-
-
+export default UniversalForm
