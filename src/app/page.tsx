af7a98b775adc7a302ffb9fcddb3ba77ae86d59d
Stanislav <Stanislavbox24@gmail.com>
2024-03-13 18:57:09 +0200

    add icon arrow, add CircularSelector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import DatePicker from '@/app/ui/helpers/datePicker'
+import CircularSelector from './ui/helpers/circularSelector';
 
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen  items-center justify-center  p-24">
-     <h1 className="text-3xl">Hello world</h1>
+    <main className="flex flex-col  min-h-screen  items-center justify-center  p-24">
+      <h1 className="text-3xl mb-[40px]">Hello world</h1>
+      <DatePicker />
+      <CircularSelector options={months} />
     </main>
   );
 }
---
+++ src/app/ui/helpers/circularSelector.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import React, { useState, useEffect, useCallback } from 'react';
+import IconArrowUp from '../icons/iconsArrowUp';
+import IconArrowDown from '../icons/iconsArrowDown';
+
+interface CircularSelectorProps {
+  options: string[];
+  onChange?: (newValue: string) => void;
+}
+
+const CircularSelector: React.FC<CircularSelectorProps> = ({ options, onChange = () => { } }) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleNext = useCallback(() => {
+    setSelectedIndex((prevIndex) => (prevIndex === options.length - 1 ? 0 : prevIndex + 1));
+  }, [options.length]);
+
+  const handlePrevious = useCallback(() => {
+    setSelectedIndex((prevIndex) => (prevIndex === 0 ? options.length - 1 : prevIndex - 1));
+  }, [options.length]);
+
+  useEffect(() => {
+    const handleScroll = (event: WheelEvent) => {
+      if (event.deltaY > 0) {
+        handleNext();
+      } else {
+        handlePrevious();
+      }
+    };
+
+    window.addEventListener('wheel', handleScroll);
+
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+    };
+  }, [handleNext, handlePrevious]);
+
+  useEffect(() => {
+    onChange(options[selectedIndex]);
+  }, [selectedIndex, options, onChange]);
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center gap-2 items-center w-36 h-40 rounded-lg shadow-lg transition-opacity duration-300">
+        <button className="flex justify-center items-center focus:outline-none transform hover:scale-110" onClick={handlePrevious}>
+          <IconArrowUp />
+        </button>
+        <div className="text-center text-lg">{options[(selectedIndex === 0 ? options.length : selectedIndex) - 1]}</div>
+        <div className="text-white bg-[#00d8d8] px-4 rounded-xl">{options[selectedIndex]}</div>
+        <div className="text-center text-lg">{options[(selectedIndex + 1) % options.length]}</div>
+        <button className="focus:outline-none text-2xl transform hover:scale-110" onClick={handleNext}>
+          <IconArrowDown />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CircularSelector;
