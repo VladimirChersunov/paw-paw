dfe108dae6fb89b69149f0834e83c84ca39eadaf
Chersunov Vladimir <kb674ua@gmail.com>
2024-03-16 01:21:11 +0200

    sleep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { DatePickerButton } from './ui/helpers/datePickerButton';
+import Link from 'next/link';
 
 export default function Home() {
 
@@ -13,7 +14,14 @@ export default function Home() {
     <main className="flex flex-col  min-h-screen  items-center justify-start  p-24">
       <h1 className="text-3xl mb-[40px]">Hello world</h1>
       <DatePickerButton/>      
-
+      <span
+          className=" mt-[120px] hover:opacity-40 cursor-pointer text-skin-link">
+          <Link href='/signin'>Sign in!</Link>
+        </span>
+        <span
+          className=" mt-[12px] hover:opacity-40 cursor-pointer text-skin-link">
+          <Link href='/signup'>Sign up!</Link>
+        </span>
     </main>
   );
 }
